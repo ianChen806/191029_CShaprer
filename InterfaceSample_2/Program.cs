b78f2af2889bc6da1389ecf27534eaa02978952f
Ian <rutsuki123@gmail.com>
2019-10-31 21:19:28 +0800

    add interfaceSample_2.

---
+++ InterfaceSample_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace InterfaceSample_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var consoleLogger = new ConsoleLogger();
+            consoleLogger.SetName("Test");
+            consoleLogger.Log("Log");
+        }
+    }
+
+    interface ILogger
+    {
+        void Log(string message);
+
+        void SetName(string name);
+    }
+
+    class ConsoleLogger : ILogger
+    {
+        private string _name;
+
+        public void Log(string message)
+        {
+            Console.WriteLine($"{_name} {message}");
+        }
+
+        public void SetName(string name)
+        {
+            _name = name;
+        }
+    }
+}
