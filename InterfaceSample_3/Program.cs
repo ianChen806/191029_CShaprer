7e3bb56f2c4daa1d6a1f76e6c02dc2311ef186cf
Ian <rutsuki123@gmail.com>
2019-10-31 21:32:16 +0800

    add interfaceSample_3.

---
+++ InterfaceSample_3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+
+namespace InterfaceSample_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var consoleLogger1 = new ConsoleLogger();
+
+            ConsoleLogger consoleLogger2 = new ConsoleLogger();
+
+            ILogger consoleLogger3 = new ConsoleLogger();
+            ILogger consoleLogger4 = new FileLogger();
+        }
+    }
+
+    interface ILogger
+    {
+        void Log(string message);
+
+        void SetName(string name);
+    }
+
+    class ConsoleLogger : ILogger
+    {
+        private string _name;
+
+        public void Log(string message)
+        {
+            Console.WriteLine($"{_name} {message}");
+        }
+
+        public void SetName(string name)
+        {
+            _name = $"Console.{name}";
+        }
+    }
+
+    class FileLogger : ILogger
+    {
+        private string _name;
+
+        public void Log(string message)
+        {
+            File.WriteAllText($"D://{_name}", message);
+        }
+
+        public void SetName(string name)
+        {
+            _name = $"File.{name}";
+        }
+    }
+}
