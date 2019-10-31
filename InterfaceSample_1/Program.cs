23bcabf6cf9c3e3063c253941065eaca25a6551b
Ian <rutsuki123@gmail.com>
2019-10-31 21:07:12 +0800

    add interfaceSample_1.

---
+++ InterfaceSample_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace InterfaceSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    interface IMyInterface
+    {
+        string Property { get; set; }
+
+        void Method();
+
+        event EventHandler Event;
+
+        string this[int index] { get; set; }
+    }
+}
