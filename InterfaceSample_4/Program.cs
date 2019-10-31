1f10261273e810c6f0679a9c73f3f7dad55bf041
Ian <rutsuki123@gmail.com>
2019-10-31 21:36:27 +0800

    add interfaceSample_4.

---
+++ InterfaceSample_4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace InterfaceSample_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var consoleLogger = new ConsoleLogger();
+
+            ILogger fileLogger = Test(consoleLogger);
+
+            fileLogger.Log("Test");
+        }
+
+        private static ILogger Test(ILogger logger)
+        {
+            logger.Log("Test");
+
+            return new FileLogger();
+        }
+    }
+
+    class FileLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            File.WriteAllText("D://Test.txt", message);
+        }
+    }
+
+    interface ILogger
+    {
+        void Log(string message);
+    }
+
+    class ConsoleLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
