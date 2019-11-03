c4eae78fdced25130fa8bf7f2d470691a7716d25
Ian <rutsuki123@gmail.com>
2019-11-03 18:21:38 +0800

    add InheritanceSample_9.

---
+++ InheritanceSample_9/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace InheritanceSample_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+
+            int name = myClass.Name("Test");
+
+            Console.WriteLine(name);
+        }
+    }
+
+    internal class MyClassBase
+    {
+        public virtual string Name(string value)
+        {
+            return "Base" + value;
+        }
+    }
+
+    internal class MyClass : MyClassBase
+    {
+        public new int Name(string value)
+        {
+            return 1;
+        }
+    }
+}
