0f53cdcb7c3473aa57a75a8c67493ff91c5c65b2
Ian <rutsuki123@gmail.com>
2019-11-03 17:19:52 +0800

    add InheritanceSample_3.

---
+++ InheritanceSample_3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace InheritanceSample_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myClassDerived = new MyClassDerived();
+        }
+    }
+
+    internal class MyClassBaseBase
+    {
+        public MyClassBaseBase()
+        {
+            Console.WriteLine("BaseBase");
+        }
+    }
+
+    internal class MyClassBase : MyClassBaseBase
+    {
+        public MyClassBase()
+        {
+            Console.WriteLine("Base");
+        }
+    }
+
+    internal class MyClassDerived : MyClassBase
+    {
+        public MyClassDerived()
+        {
+            Console.WriteLine("Derived");
+        }
+    }
+}
