62ab18d3bb65742237bc5e2f16640fc289bc97e7
Ian <rutsuki123@gmail.com>
2019-11-03 15:31:58 +0800

    add InterfaceSample_6

---
+++ InterfaceSample_6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace InterfaceSample_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myClass = new MyClass();
+        }
+    }
+
+    interface IMyInterface
+    {
+        string Name { get; set; }
+    }
+
+    interface IMyInterface2
+    {
+        int Id { get; set; }
+
+        string Name { get; set; }
+    }
+
+    internal class MyClass : IMyInterface, IMyInterface2
+    {
+        public int Id { get; set; }
+
+        string IMyInterface2.Name { get; set; }
+
+        string IMyInterface.Name { get; set; }
+    }
+}
