31845bf8e7d1be3e18de62ac3b546a0553ad855d
Ian <rutsuki123@gmail.com>
2019-11-03 18:17:00 +0800

    add InheritanceSample_8.

---
+++ InheritanceSample_8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace InheritanceSample_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+
+            var name = myClass.Name();
+
+            Console.WriteLine(name);
+        }
+    }
+
+    internal class MyClassBase
+    {
+        public virtual string Name()
+        {
+            return "Base";
+        }
+    }
+
+    internal class MyClass : MyClassBase
+    {
+        public new string Name()
+        {
+            return "Class";
+        }
+    }
+}
