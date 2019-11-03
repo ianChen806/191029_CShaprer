e944cf8eaf6f378a95792384352f6fa43734e366
Ian <rutsuki123@gmail.com>
2019-11-03 18:27:43 +0800

    add InheritanceSample_11.

---
+++ InheritanceSample_11/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace InheritanceSample_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    internal class MyClassBase
+    {
+        public virtual string Name()
+        {
+            return "Name";
+        }
+    }
+
+    internal class MyClass : MyClassBase
+    {
+        public sealed override string Name()
+        {
+            return "Class";
+        }
+    }
+}
