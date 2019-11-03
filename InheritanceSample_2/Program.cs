cb01d9079d4774af6f3cf03a1fb0d1584ca95407
Ian <rutsuki123@gmail.com>
2019-11-03 16:36:32 +0800

    add InheritanceSample_2.

---
+++ InheritanceSample_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace InheritanceSample_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myClass = new MyClass();
+        }
+    }
+
+    internal class MyClass
+    {
+        public string Name()
+        {
+            return "Name";
+        }
+    }
+}
