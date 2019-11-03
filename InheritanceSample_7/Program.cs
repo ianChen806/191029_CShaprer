fcb8e8dbb33b1265a2cab55a24f9805b40cad867
Ian <rutsuki123@gmail.com>
2019-11-03 18:05:01 +0800

    add InferitanceSample7.

---
+++ InheritanceSample_7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace InheritanceSample_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myClass = new MyClass();
+
+            var name = myClass.Name();
+
+            Console.WriteLine(name);
+        }
+    }
+
+    internal class MyClassBase
+    {
+        public virtual string Name()
+        {
+            return "Base";
+        }
+    }
+
+    internal class MyClass : MyClassBase
+    {
+        public string Name()
+        {
+            return "Class";
+        }
+    }
+}
