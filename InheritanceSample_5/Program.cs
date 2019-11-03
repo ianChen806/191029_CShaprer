efc90a42708bfc300908ada890cc8834f1693aaf
Ian <rutsuki123@gmail.com>
2019-11-03 17:36:53 +0800

    adjust InheritanceSample_5.

---
+++ InheritanceSample_5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace InheritanceSample_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myClass = new MyClass();
+
+            var name = myClass.Name();
+            
+            Console.WriteLine(name);
+        }
+    }
+
+    internal class MyClassBase
+    {
+        public virtual string Name()
+        {
+            return "Base";
+        }
+    }
+
+    internal class MyClass : MyClassBase
+    {
+        public override string Name()
+        {
+            Console.WriteLine("Class");
+            return base.Name();
+        }
+    }
+}
