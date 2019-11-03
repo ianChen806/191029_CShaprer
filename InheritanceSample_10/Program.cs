8214caeea456dae33b29d99e4a3e1169ec97eafe
Ian <rutsuki123@gmail.com>
2019-11-03 18:22:56 +0800

    rename InheritanceSample.

---
+++ InheritanceSample_10/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InheritanceSample_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
