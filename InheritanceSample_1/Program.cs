3f04b3bd1144057e51d9e4bb6501fb63e06f1b02
Ian <rutsuki123@gmail.com>
2019-11-03 16:30:11 +0800

    add InheritanceSample_1.

---
+++ InheritanceSample_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace InheritanceSample_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myClass2 = new MyClass2();
+
+            Console.WriteLine(myClass2.Name());
+        }
+    }
+
+    class MyClass1
+    {
+        public string Name()
+        {
+            return "Name";
+        }
+    }
+
+    class MyClass2 : MyClass1
+    {
+    }
+}
