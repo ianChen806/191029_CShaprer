91166e16239849406a2063dc9246ac10fef64a4c
Ian <rutsuki123@gmail.com>
2019-11-03 18:31:52 +0800

    add TypeConvertSample_1.

---
+++ TypeConvertSample_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TypeConvertSample_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClassBase myClass1 = new MyClass1();
+
+            var myClass2 = (MyClass2)myClass1;
+
+            var myClass3 = myClass1 as MyClass2;
+        }
+    }
+
+    internal class MyClassBase
+    {
+    }
+
+    internal class MyClass1 : MyClassBase
+    {
+    }
+
+    internal class MyClass2 : MyClassBase
+    {
+    }
+}
