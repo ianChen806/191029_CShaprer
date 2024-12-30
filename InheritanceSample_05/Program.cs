cc1d8993b266371656af8b450557c1e621f1c698
IanChen <ian806.chen@gmail.com>
2024-12-30 21:01:44 +0800

    adjust

--- InheritanceSample_05/Program.cs
+++ InheritanceSample_05/Program.cs
@@ -8,6 +8,7 @@
             MyClassBase myClass1 = derived1;
 
             MyClassBase myClassBase = Test(derived1);
+            MyClassBase myClassBase2 = Test2(derived1);
 
             MyClassDerived2 derived2 = new MyClassDerived2();
 
@@ -17,6 +18,11 @@
         private static MyClassBase Test(MyClassBase myClass)
         {
             return myClass;
+        }
+
+        private static MyClassBase Test2(MyClassDerived1 derived1)
+        {
+            return derived1;
         }
     }
 
