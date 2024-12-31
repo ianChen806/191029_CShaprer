db4e84e248512c3523eed148012d7ba444a3185e
IanChen <ian806.chen@gmail.com>
2024-12-31 10:28:12 +0800

    add Exercise

---
+++ Exercise/InterfaceExercise.cs
@@ -0,0 +1,34 @@
+using FluentAssertions;
+
+namespace Exercise;
+
+public class InterfaceExercise
+{
+    /*
+     * 實做一個 工廠
+     * 依照傳入 類型 返回不同的 Exporter
+     * 目前支援 CSV 和 Excel
+     * 呼叫 Exporter 的方法 "Export" 會返回 "Export CSV" 或 "Export Excel"
+     */
+    [Fact]
+    public void try_Factory()
+    {
+        var factory = new ExporterFactory();
+
+        // var csv = factory.Create("csv");
+        // csv.Should().BeOfType<CsvExporter>();
+        // csv.Export().Should().Be("Export CSV");
+        //
+        // var excel = factory.Create("excel");
+        // excel.Should().BeOfType<ExcelExporter>();
+        // excel.Export().Should().Be("Export Excel");
+    }
+}
+
+public class ExporterFactory
+{
+    // public IExcelExporter Create(string csv)
+    // {
+    //     throw new NotImplementedException();
+    // }
+}
